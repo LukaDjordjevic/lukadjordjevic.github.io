93cd453cd124ad095431d40d34145fa1c40a807a
Luka <lulo.djordjevic@gmail.com>
2022-09-03 11:44:34 +0200

    Updates

---
+++ precache-manifest.c849c32ec0f1e4eeb0c4ece77288ca6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b14c0b2ee1d921baefa",
+    "url": "/static/css/main.b11bc2b5.chunk.css"
+  },
+  {
+    "revision": "9b14c0b2ee1d921baefa",
+    "url": "/static/js/main.9b14c0b2.chunk.js"
+  },
+  {
+    "revision": "079a51e1eca575ceb4bf",
+    "url": "/static/js/1.079a51e1.chunk.js"
+  },
+  {
+    "revision": "acca8f821dbfe87e295e",
+    "url": "/static/js/2.acca8f82.chunk.js"
+  },
+  {
+    "revision": "fa5df98263ff31e4333f",
+    "url": "/static/js/runtime~main.fa5df982.js"
+  },
+  {
+    "revision": "2ca6bb7d8619e68a51bf59dcb90d7f09",
+    "url": "/index.html"
+  }
+];
